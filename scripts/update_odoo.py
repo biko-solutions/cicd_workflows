2cec07ce7d677608d10f4e660c07c453ec88d460
Borovlev A. S <bas@qdes.com.ua>
2024-11-08 21:39:36 +0200

    Refactor log tailing in update_odoo.py to simplify process management and improve readability

--- scripts/update_odoo.py
+++ scripts/update_odoo.py
@@ -19,55 +19,15 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def tail_log_file(log_file, stop_event, max_wait_time=30):
+def tail_log_file(log_file, proc):
     """Function to tail a log file and print the output to the logger."""
 
-    # waiting for log-file to be created
-    wait_time = 0
-    while not os.path.exists(log_file) and wait_time < max_wait_time:
-        logger.debug(
-            f"[DEBUG] File {log_file} does not exist yet, waiting for its creation..."
-        )
-        time.sleep(1)
-        wait_time += 1
-
-    if not os.path.exists(log_file):
-        logger.error(
-            f"[ERROR] Log file {log_file} was not created after waiting for {max_wait_time} seconds."
-        )
-        return
-
-    logger.debug("[DEBUG] Log file found, starting tail -f")
-
-    with subprocess.Popen(
-        ["tail", "-f", log_file],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-    ) as proc:
-        index = 0
-        logger.debug("[DEBUG] tail -f process started")
-
-        while not stop_event.is_set():
-            logger.debug(f"[DEBUG] Checking before reading line, iteration {index}")
-            index += 1
-
-            # Check the process state
-            if proc.poll() is not None:
-                logger.debug("[DEBUG] tail process has finished")
-                break
-
-            line = proc.stdout.readline()
-            if line:
-                logger.debug("[DEBUG] Line received from stdout stream")
-                logger.info(f"[LOG OUTPUT] {line.strip()}")
-            else:
-                logger.debug("[DEBUG] Line is empty, possibly end of stream")
-                break
-
-        # Terminate the process and log the message
-        proc.terminate()
-        logger.debug("[DEBUG] tail process terminated manually")
+    while proc.poll() is None:  # While the process is active
+        line = proc.stdout.readline()
+        if line:
+            logger.info(f"[LOG OUTPUT] {line.strip()}")
+        else:
+            time.sleep(0.1)
 
 
 def update_database(
@@ -107,12 +67,15 @@ def update_database(
     logger.info(f"Starting database update for {db_name}")
     logger.info(f"Logfile {log_file}")
 
-    # Флаг для остановки потока
-    stop_event = threading.Event()
-
-    # Запуск потока для параллельного чтения логов, если show_log == True
+    log_proc = None
     if show_log:
-        log_thread = threading.Thread(target=tail_log_file, args=(log_file, stop_event))
+        log_proc = subprocess.Popen(
+            ["tail", "-f", log_file],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+        log_thread = threading.Thread(target=tail_log_file, args=(log_file, log_proc))
         log_thread.start()
 
     try:
@@ -124,16 +87,17 @@ def update_database(
         logger.error(f"Failed to update database {db_name}. Error: {e}")
         error_found = True
     finally:
-        # Установка флага для остановки потока и ожидание его завершения
-        if show_log:
-            stop_event.set()
-            log_thread.join()
+        # Forcefully terminate tail -f
+        if show_log and log_proc:
+            log_proc.terminate()  # Terminate tail -f process
+            log_thread.join()  # Wait for the thread to finish
+            logger.debug("[DEBUG] Tail process forcefully terminated")
 
     # Always check logs for errors, regardless of the process exit code
     with open(log_file, "r") as file:
         log_content = file.read()
 
-        if re.search(r" ERROR |Traceback", log_content, re.IGNORECASE):
+        if re.search(r" ERROR |Traceback", log_content):
             logger.error(
                 f"Errors found during the update of database {db_name}. Check the logs for more information."
             )
